6f73962e304615b9a2c342e808163e9bd97c4ef7
StarSwallowedDeep <139122242+StarSwallowedDeep@users.noreply.github.com>
2026-01-08 20:14:03 +0900

    refactor(message-board): remove DTO mapping logic from MessageBoard entity

--- src/main/java/com/notecurve/messageboard/domain/MessageBoard.java
+++ src/main/java/com/notecurve/messageboard/domain/MessageBoard.java
@@ -1,21 +1,23 @@
 package com.notecurve.messageboard.domain;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import com.notecurve.user.domain.User;
-import com.notecurve.messageboard.dto.CommentDTO;
-
-import lombok.Getter;
-import lombok.Setter;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import com.notecurve.user.domain.User;
+import com.notecurve.messageboard.dto.CommentDTO;
 
 @Entity
 @Getter
@@ -44,12 +46,6 @@ public class MessageBoard {
     @OneToMany(mappedBy = "messageBoard", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonManagedReference
     private List<Comment> comments = new ArrayList<>();
-
-    public List<CommentDTO> getCommentsDTO() {
-        return comments.stream()
-                .map(comment -> new CommentDTO(comment.getId(), comment.getContent(), comment.getUserDTO()))
-                .collect(Collectors.toList());
-    }
 
     public String getFormattedCreatedAt() {
         ZoneId seoulZone = ZoneId.of("Asia/Seoul");
