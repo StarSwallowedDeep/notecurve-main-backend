3a789bc42da0213f705d78f53c622bd45cea555b
StarSwallowedDeep <139122242+StarSwallowedDeep@users.noreply.github.com>
2026-01-08 20:17:34 +0900

    refactor(comment): add @NotBlank validation to CommentRequest

--- src/main/java/com/notecurve/messageboard/dto/CommentRequest.java
+++ src/main/java/com/notecurve/messageboard/dto/CommentRequest.java
@@ -1,10 +1,14 @@
 package com.notecurve.messageboard.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class CommentRequest {
+
+    @NotBlank
     private String content;
 }
