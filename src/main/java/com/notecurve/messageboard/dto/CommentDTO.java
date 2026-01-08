5645f078dce622cfa5aaf72a5e920b8ba728c747
StarSwallowedDeep <139122242+StarSwallowedDeep@users.noreply.github.com>
2026-01-08 20:15:55 +0900

    refactor(comment): replace user info with delete permission in CommentDTO

--- src/main/java/com/notecurve/messageboard/dto/CommentDTO.java
+++ src/main/java/com/notecurve/messageboard/dto/CommentDTO.java
@@ -1,13 +1,13 @@
 package com.notecurve.messageboard.dto;
 
-import com.notecurve.user.dto.UserDTO;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class CommentDTO {
+    
     private Long id;
     private String content;
-    private UserDTO user;
+    private boolean canDelete;
 }
