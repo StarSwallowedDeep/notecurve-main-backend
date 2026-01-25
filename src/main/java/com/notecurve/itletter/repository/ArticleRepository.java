e318b9b095466ff4a4262c04306d06ea84a6ed5b
StarSwallowedDeep <139122242+StarSwallowedDeep@users.noreply.github.com>
2026-01-26 00:49:08 +0900

    feat(itletter): add repository method to fetch latest article by category

--- src/main/java/com/notecurve/itletter/repository/ArticleRepository.java
+++ src/main/java/com/notecurve/itletter/repository/ArticleRepository.java
@@ -3,5 +3,9 @@ package com.notecurve.itletter.repository;
 import com.notecurve.itletter.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+    Optional<Article> findTopByCategoryOrderByIdDesc(String category);
 }
