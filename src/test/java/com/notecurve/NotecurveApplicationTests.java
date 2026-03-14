eac1767bf7479b9cb6e1462d5deaf9dab1f20a1d
StarSwallowedDeep <139122242+StarSwallowedDeep@users.noreply.github.com>
2026-03-14 15:52:24 +0900

    Update NotecurveApplicationTests.java

--- src/test/java/com/notecurve/NotecurveApplicationTests.java
+++ src/test/java/com/notecurve/NotecurveApplicationTests.java
@@ -1,9 +1,7 @@
 package com.notecurve;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class NotecurveApplicationTests {
 
 	@Test
