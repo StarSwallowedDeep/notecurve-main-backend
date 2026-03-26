c16e17c420a8df54633c4c4dc249400c6ad78bc7
StarSwallowedDeep <139122242+StarSwallowedDeep@users.noreply.github.com>
2026-03-27 00:36:54 +0900

    feat(message-board): add bulk delete method by user in MessageBoardRepository

--- src/main/java/com/notecurve/messageboard/repository/MessageBoardRepository.java
+++ src/main/java/com/notecurve/messageboard/repository/MessageBoardRepository.java
@@ -3,6 +3,7 @@ package com.notecurve.messageboard.repository;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.notecurve.messageboard.domain.MessageBoard;
+import com.notecurve.user.domain.User;
 
 import java.util.List;
 
@@ -11,4 +12,6 @@ public interface MessageBoardRepository extends JpaRepository<MessageBoard, Long
     // @EntityGraph를 사용하여 MessageBoard와 관련된 Comments를 함께 로딩
     @EntityGraph(attributePaths = "comments")
     List<MessageBoard> findAll();
+
+    void deleteByUser(User user);
 }
