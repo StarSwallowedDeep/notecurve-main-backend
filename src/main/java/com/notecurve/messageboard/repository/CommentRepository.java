27f983ddddf450c90af6c482de64ca45dc495aee
StarSwallowedDeep <139122242+StarSwallowedDeep@users.noreply.github.com>
2026-04-26 18:44:49 +0900

    feat(comment): add deleteByUserId query method to CommentRepository

--- src/main/java/com/notecurve/messageboard/repository/CommentRepository.java
+++ src/main/java/com/notecurve/messageboard/repository/CommentRepository.java
@@ -3,6 +3,11 @@ package com.notecurve.messageboard.repository;
 import java.util.List;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.notecurve.messageboard.domain.Comment;
 import com.notecurve.messageboard.domain.MessageBoard;
 import com.notecurve.user.domain.User;
@@ -18,4 +23,9 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     void deleteByUser(User user);
 
     void deleteByMessageBoard(MessageBoard messageBoard);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Comment c WHERE c.user.id = :userId")
+    void deleteByUserId(@Param("userId") Long userId);
 }
